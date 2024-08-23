eaeaa4573bd5a432b5153188de21c7adb9f69909
roshan4182 <149868016+roshan4182@users.noreply.github.com>
2024-08-23 22:23:32 +0200

    Update SVM.py

--- MODEL_TRAINING/SVM.py
+++ MODEL_TRAINING/SVM.py
@@ -4,35 +4,35 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 
-# Load your dataset
+# Loading  dataset
 df = pd.read_csv('/Users/roshan1610/Desktop/sentiment-analysis-env/final_data.csv')
 
-# Drop rows with missing 'normalized_text'
+# Dropping rows with missing 'normalized_text'
 df = df.dropna(subset=['normalized_text'])
 
-# Split into features and labels
+# Splitting into features and labels
 X = df['normalized_text']
 y = df['label']
 
-# Split into training and testing sets
+# Splitting into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Fill NaN values with an empty string (just in case)
+# Filling NaN values with an empty string
 X_train = X_train.fillna('')
 X_test = X_test.fillna('')
 
-# Convert text data into TF-IDF features
+# Converting text data into TF-IDF features
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-# Support Vector Machine (SVM) Model
+# Supportting Vector Machine (SVM) Model
 svm_model = SVC()
 svm_model.fit(X_train_tfidf, y_train)
 y_pred_svm = svm_model.predict(X_test_tfidf)
 print("SVM Classification Report")
 print(classification_report(y_test, y_pred_svm))
-# Save SVM results
+# at last saving  SVM results
 with open('/Users/roshan1610/Desktop/sentiment-analysis-env/svm_results.txt', 'w') as f:
     f.write("SVM Classification Report\n")
     f.write(classification_report(y_test, y_pred_svm))
