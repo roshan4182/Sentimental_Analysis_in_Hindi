5aae136bd44a39816a03ed49017c83f34c0c7705
roshan4182 <149868016+roshan4182@users.noreply.github.com>
2024-08-23 22:26:14 +0200

    Update XGBoost.py

--- MODEL_TRAINING/XGBoost.py
+++ MODEL_TRAINING/XGBoost.py
@@ -6,24 +6,24 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 
-# Load your dataset
+# Loading your dataset
 df = pd.read_csv('/Users/roshan1610/Desktop/sentiment-analysis-env/final_data.csv')
 
-# Drop rows with missing 'normalized_text'
+# Dropping rows with missing 'normalized_text'
 df = df.dropna(subset=['normalized_text'])
 
-# Split into features and labels
+# Splitting into features and labels
 X = df['normalized_text']
 y = df['label']
 
-# Split into training and testing sets
+# Splitting into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Fill NaN values with an empty string (just in case)
+# Filling NaN values with an empty string 
 X_train = X_train.fillna('')
 X_test = X_test.fillna('')
 
-# Convert text data into TF-IDF features
+# Converting text data into TF-IDF features
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
@@ -34,7 +34,7 @@ xgb_model.fit(X_train_tfidf, y_train)
 y_pred_xgb = xgb_model.predict(X_test_tfidf)
 print("XGBoost Classification Report")
 print(classification_report(y_test, y_pred_xgb))
-# Save XGBoost results
+# Saving XGBoost results
 with open('/Users/roshan1610/Desktop/sentiment-analysis-env/xgboost_results.txt', 'w') as f:
     f.write("XGBoost Classification Report\n")
     f.write(classification_report(y_test, y_pred_xgb))
